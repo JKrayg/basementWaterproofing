70e548946ff6471e6025c7ca1583d91c007adcf8
Jake Krayger <jkrayg@protonmail.com>
2020-08-21 12:38:05 -0400

    basement dropdown / images fixed

--- script/script.js
+++ script/script.js
@@ -1,14 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.modal').hide();
 
-    $("#permanentlyStabilize1").on("click", function() {
-        $('.modal').show();
-        $("#modalPhoto1").attr("src", "style/images/Scan 1.png");
-        $("#modalPhoto2").attr("src", "style/images/Scan.png");
-
-    })
-
-    $("#pushPier").on("click", function() {
+    $("#pushPier").on("click", function () {
         $('.modal').show();
         $("#modalPhoto1").attr("src", "style/images/Scan 1.png");
         $("#modalPhoto2").attr("src", "style/images/Scan.png");
@@ -20,25 +13,38 @@ $(document).ready(function() {
         location.reload();
     })
 
-    $("#stabilizeAndSecure").on("click", function() {
+    $("#geoLock").on("click", function () {
+        $('.modal').show();
+        $("#modalPhoto1").attr("src", "style/images/permstabilize1.png");
+        $("#modalPhoto2").attr("src", "style/images/permstabilize2.png");
+    })
+
+    $("#powerBrace").on("click", function () {
+        $('.modal').show();
+        $("#modalPhoto1").attr("src", "style/images/permstabilize3.png");
+        $("#modalPhoto2").attr("src", "style/images/permstabilize4.png");
+    })
+
+    $("#carbonOmor").on("click", function () {
         $('.modal').show();
         $("#modalPhoto1").attr("src", "style/images/stabilizennsecure.png");
         $("#modalPhoto2").attr("src", "style/images/stabilizensecure1.png");
+    })
 
     // })
 
     // $("#permanentlyStabilize").on("click", function() {
-        $('.modal').show();
-        $(".carousel-indicators").append("<li data-target='#carouselExampleIndicators' data-slide-to='2'></li>" + 
-        "<li data-target='#carouselExampleIndicators' data-slide-to='3'></li>")
-        $(".carousel-inner").append("<div class = 'carousel-item photo3'>")
-        $(".photo3").append("<img id = 'modalPhoto3' class='d-block w-100'>")
-        $(".carousel-inner").append("<div class = 'carousel-item photo4'>")
-        $(".photo4").append("<img id = 'modalPhoto4' class='d-block w-100'>")
-        $("#modalPhoto1").attr("src", "style/images/permstabilize1.png");
-        $("#modalPhoto2").attr("src", "style/images/permstabilize2.png");
-        $("#modalPhoto3").attr("src", "style/images/permstabilize3.png");
-        $("#modalPhoto4").attr("src", "style/images/permstabilize4.png");
+    //     $('.modal').show();
+    //     $(".carousel-indicators").append("<li data-target='#carouselExampleIndicators' data-slide-to='2'></li>" + 
+    //     "<li data-target='#carouselExampleIndicators' data-slide-to='3'></li>")
+    //     $(".carousel-inner").append("<div class = 'carousel-item photo3'>")
+    //     $(".photo3").append("<img id = 'modalPhoto3' class='d-block w-100'>")
+    //     $(".carousel-inner").append("<div class = 'carousel-item photo4'>")
+    //     $(".photo4").append("<img id = 'modalPhoto4' class='d-block w-100'>")
+    //     $("#modalPhoto1").attr("src", "style/images/permstabilize1.png");
+    //     $("#modalPhoto2").attr("src", "style/images/permstabilize2.png");
+    //     $("#modalPhoto3").attr("src", "style/images/permstabilize3.png");
+    //     $("#modalPhoto4").attr("src", "style/images/permstabilize4.png");
 
-    })
+    // })
 });
